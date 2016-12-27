2b6e7095bae539707f0b1f0d484814a9e9633831
Kevin Duraj <kduraj@gmail.com>
2016-12-26 16:05:16 -0800

    2016-12-26 16:05

--- src/main/scala/YoutubeVideos.scala
+++ src/main/scala/YoutubeVideos.scala
@@ -43,7 +43,7 @@ object YoutubeVideos {
         val output_prefix   = args(1); println("output_prefix=" + output_prefix)
 
         /*--- Open 100 ranked batchFiles with append flag set to true, ---*/
-        for(io <- 0 to 200) { batchFiles +=  new BufferedWriter(new FileWriter(new File(output_prefix + "/video_" + io +".dat"), true)) }
+        for(io <- 0 to 100) { batchFiles +=  new BufferedWriter(new FileWriter(new File(output_prefix + "/video_" + io +".dat"), true)) }
 
         if(args(0) == "export_data") {
             export_data();
@@ -52,7 +52,7 @@ object YoutubeVideos {
         }
 
         /*--- Close ranking batchFiles ---*/
-        for(ic <- 0 to 200) { batchFiles(ic).close() }
+        for(ic <- 0 to 100) { batchFiles(ic).close() }
     }
     /*---------------------------------------------------------------------------------------------------------------------------------------------------*/
     val user_defined_function = udf(( 
@@ -61,121 +61,10 @@ object YoutubeVideos {
                 scoreLikes:     String,
                 scoreViews:     String ) => { 
 
-            var resPeriod   = "up3years"
-
-            var resDuration = "" 
-            var resDrugs    = "" 
-            var resLanguage = ""
-            var resSex      = ""
-            var resViolence = ""
-            var resViews    = ""
-
-            var period      = 0.0
-            var res30day    = 0
-            var totalRank   = 0.0
-            var globalRank  = 0.0
-
-            /*------------------------- [ published_at ] -----------------------------*/
-            try {
-                val beginDate   = published_at.toString.take(10)
-                    val dateFormat  = new SimpleDateFormat("yyyy-MM-dd");
-                val date        = new Date();
-                val currentDate = dateFormat.format(date)
-
-                    val formatter   = DateTimeFormatter.ofPattern("yyyy-MM-dd")
-                    val oldDate     = LocalDate.parse(beginDate, formatter)
-                    val newDate     = LocalDate.parse(currentDate, formatter)
-                    period          = newDate.toEpochDay() - oldDate.toEpochDay()
-
-                    if(period > (365*3))      { resPeriod = "over3years";           }
-                    else if(period > (365*2)) { resPeriod = "up3years";             }
-                    else if(period > (365))   { resPeriod = "up2years up3years";    }
-                    else                      { resPeriod = "up1year up3years";     }
-
-                if(period <= 30)          { resPeriod = resPeriod + " up1month" }
-                if(period <= 14)          { resPeriod = resPeriod + " up2weeks" }
-                if(period <= 7)           { resPeriod = resPeriod + " up1week"  }
-
-
-                if (period > 2555) { totalRank +=  1  }
-                else if (period > 2529) { totalRank +=  2  }
-                else if (period > 2503) { totalRank +=  3  }
-                else if (period > 2478) { totalRank +=  4  }
-                else if (period > 2452) { totalRank +=  6  }
-
-            } catch { case e: Exception => { resPeriod = "over3years" } }
-
-    //totalRank  = 200 - (log2(period) * 10)
-
-
-    /*******************************************************************************************
-      X_FILTER_MAP = {'3-years-old-filter': "up3years", 
-      "brandsafe-language": "6429f",
-      "brandsafe-drugs/alcohol": "2c2ee",
-      "brandsafe-violence": "0dd8f",
-      "brandsafe-nudity": "68876",
-      "10k-lifetime-views": "0e88c",
-      "30-seconds-duration": "ae363"
-      }
-     *******************************************************************************************/
-
-    /*------------------------- [ duration ] --------------------------------*/
-    try {
-        val b = duration.toDouble
-            if(b > 30.0 ) { resDuration = "ae363"  }
-        totalRank += log2(b) 
-
-    } catch {  case e: Exception => { resDuration = "ae363" }   }
-
-
-    /*------------------------- [ likes ] --------------------------------*/
-    try {
-        val likes = scoreLikes.toDouble
-
-            if (likes < 2) { totalRank +=  1  }
-            else if (likes < 3) { totalRank +=  2  }
-            else if (likes < 7) { totalRank +=  3  }
-            else if (likes < 14) { totalRank +=  4  }
-            else if (likes < 23) { totalRank +=  6  }
-            else if (likes < 35) { totalRank +=  7  }
-            else if (likes < 50) { totalRank +=  8  }
-            else if (likes < 67) { totalRank +=  9  }
-            else if (likes < 87) { totalRank +=  10  }
-
-    } catch {  case e: Exception => { totalRank += 1 }   }
-
-    /*--------------------------------------------------------------------*/
-    /*                           [ VIEWS ]                                */
-    /*--------------------------------------------------------------------*/
-    try {
-        val views = scoreViews.toDouble
-            if(views > 9999 ) { resViews = "0e88c" } 
-
-        if (views < 2) { totalRank +=  1  }
-        else if (views < 3) { totalRank +=  2  }
-        else if (views < 7) { totalRank +=  3  }
-        else if (views < 13) { totalRank +=  5  }
-        else if (views < 21) { totalRank +=  6  }
-        else if (views < 31) { totalRank +=  7  }
-        else if (views < 43) { totalRank +=  9  }
-        else if (views < 57) { totalRank +=  10  }
-
-
-    } catch {  case e: Exception => { totalRank += 1 }   }
-
-    /*********************************************************************************************/
-
-
     /*--------------------- [ Normalize Result ] -----------------------------*/
     val random  = scala.util.Random
-    try {
-        if(totalRank <  10) { totalRank = random.nextInt(10)      }
-        if(totalRank > 200) { totalRank = 191+ random.nextInt(10) }
-    } catch {  case e: Exception => { totalRank = random.nextInt(10)  }  }
-
-    // Return string from function
-    //resPeriod + " days" + period.toInt.toString() + " xrank" + totalRank.toInt.toString() + " " + resDrugs    + " " + resLanguage + " " + resSex      + " " + resViolence + " " + resViews    + " " +  resDuration
-    "xrank" + totalRank.toInt.toString() + " " +  resPeriod + " days" + period.toInt.toString() + " " + resDrugs    + " " + resLanguage + " " + resSex      + " " + resViolence + " " + resViews    + " " +  resDuration
+    var totalRank = random.nextInt(100)
+    "xrank" + totalRank.toInt.toString() + " "
 
     })
 
@@ -187,13 +76,14 @@ object YoutubeVideos {
         val spark = SparkSession.builder().appName("YoutubeVideos").config("spark.some.config.option", "some-value").getOrCreate()
             import spark.implicits._
 
-            val df1 = spark.read.format("org.apache.spark.sql.cassandra").options(Map( "table" -> "video2", "keyspace" -> "youtube" )).load().toDF()
+            //val df1 = spark.read.format("org.apache.spark.sql.cassandra").options(Map( "table" -> "video2", "keyspace" -> "youtube" )).load().toDF()
+            val df1 = spark.read.format("org.apache.spark.sql.cassandra").options(Map( "table" -> "video2", "keyspace" -> "youtube" )).load()
             df1.printSchema()
-//            df1.createOrReplaceTempView("video")
+            df1.createOrReplaceTempView("video")
 
-//            val video1 = spark.sql("SELECT  video_id, channel_id, channel_text, channel_title, duration, stats_comments, stats_dislikes, stats_favorite," +
-//                    " stats_likes, stats_views, topics, topics_relevant, ts_video_published, video_category_id, video_language, " +
-//                    " video_seconds, video_tags, video_text, video_title FROM video").toDF()
+            val video1 = spark.sql("SELECT  video_id, channel_id, channel_text, channel_title, duration, stats_comments, stats_dislikes, stats_favorite," +
+                    " stats_likes, stats_views, topics, topics_relevant, ts_video_published, video_category_id, video_language, " +
+                    " video_seconds, video_tags, video_text, video_title FROM video WHERE video_title IS NOT NULL").toDF()
 
             val video2 = df1.withColumn("hashtags", user_defined_function (
                         col("ts_video_published"), 
@@ -203,9 +93,9 @@ object YoutubeVideos {
                         ))
 
             video2.map(t =>  
-                    "video_id="          + t.getAs[String]("video_id")                                       + "\n" +
-                    "video_title="       + t.getAs[String]("video_text")                                     + "\n" +
-                    "video_text="        + t.getAs[String]("video_text").replaceAll("(\\<|\\>|\\(|\\)|/|\"|=|-|\\\\|\\.\\.\\.|\\p{C})", " ") + "\n"
+                    "video_id="          + t.getAs[String]("video_id")                                       + "\n" 
+                    //"video_title="       + t.getAs[String]("video_text")                                     + "\n" +
+                    //"video_text="        + t.getAs[String]("video_text").replaceAll("(\\<|\\>|\\(|\\)|/|\"|=|-|\\\\|\\.\\.\\.|\\p{C})", " ") + "\n"
                     ).collect().map(_.trim).foreach( row => { 
 
                         var mapFile = 0
